6a3ebab528a31258654f7214ed4214e5936104e9
haloman30 <dillonlance15@yahoo.com>
2023-08-13 23:10:37 -0500

    Formatting improvements, hopefully fix build issue

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
     {
         title: 'New & Improved',
-        Svg: require('@site/static/img/new_and_improved.png').default,
+        png: require('@site/static/img/new_and_improved.png').default,
         description: (
             <>
                 DeviantCord 4 was overhauled to be much more reliable then DeviantCord 4. With less crashes and maintenance.
@@ -14,45 +14,45 @@ const FeatureList = [
         ),
     },
     {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/easy_to_use.png').default,
-    description: (
-      <>
-        DeviantCord 4 was overhauled to uses Discords Interactive components such as button
-          to make it easier to use!
-      </>
-    ),
-  },
-  {
-    title: 'Focus on your community!',
-    Svg: require('@site/static/img/community.png').default,
-    description: (
-      <>
-        DeviantCord lets you focus on your art, and we&apos;ll notify your Discord Community about new posts.
-      </>
-    ),
-  },
+        title: 'Easy to Use',
+        png: require('@site/static/img/easy_to_use.png').default,
+        description: (
+            <>
+                DeviantCord 4 was overhauled to uses Discords Interactive components such as button
+                to make it easier to use!
+            </>
+      ),
+    },
+    {
+        title: 'Focus on your community!',
+        png: require('@site/static/img/community.png').default,
+        description: (
+            <>
+                DeviantCord lets you focus on your art, and we&apos;ll notify your Discord Community about new posts.
+            </>
+        ),
+    },
     {
         title: 'Fast Notifications',
-        Svg: require('@site/static/img/fast_notifications.png').default,
+        png: require('@site/static/img/fast_notifications.png').default,
         description: (
             <>
                 DeviantCord 4 introduces a faster notification system. DeviantCord delayed notifications are a thing of the past!
             </>
         ),
     },
-  {
-    title: 'Self Hosted Available',
-    Svg: require('@site/static/img/self_hosted.png').default,
-    description: (
-      <>
-        DeviantCord 4 allows for you to self host your own DeviantCord bot. It works great with CockroachDB Serverless, which offers a free tier!
-      </>
-    ),
-  },
+    {
+        title: 'Self Hosted Available',
+        png: require('@site/static/img/self_hosted.png').default,
+        description: (
+            <>
+                DeviantCord 4 allows for you to self host your own DeviantCord bot. It works great with CockroachDB Serverless, which offers a free tier!
+            </>
+        ),
+    },
     {
         title: 'Free & and better then DAs offering',
-        Svg: require('@site/static/img/free.png').default,
+        png: require('@site/static/img/free.png').default,
         description: (
             <>
                 DeviantCord 4 is free and will never have its core features behind a paywall.
@@ -67,6 +67,20 @@ function Feature({Svg, title, description}) {
     <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function Feature({png, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img src="{png}" role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
